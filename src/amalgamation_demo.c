9333a16e88daa361ddde40b91501946fe801d378
Yanzhe Lu <ericyzlu@cedar5.int.cedar.computecanada.ca>
2025-06-11 01:25:11 -0700

    add submodule PFOR

---
+++ src/amalgamation_demo.c
@@ -0,0 +1,12 @@
+/* auto-generated on Tue 10 Jun 2025 08:08:02 PM PDT. Do not edit! */
+
+#include <stdio.h>
+#include "roaring.c"
+int main() {
+  roaring_bitmap_t *r1 = roaring_bitmap_create();
+  for (uint32_t i = 100; i < 1000; i++) roaring_bitmap_add(r1, i);
+  printf("cardinality = %d\n", (int) roaring_bitmap_get_cardinality(r1));
+  roaring_bitmap_free(r1);
+  return 0;
+}
+
---
+++ src/amalgamation_demo.cpp
@@ -0,0 +1,20 @@
+/* auto-generated on Tue 10 Jun 2025 08:08:02 PM PDT. Do not edit! */
+
+#include <iostream>
+#include "roaring.hh"
+#include "roaring.c"
+int main() {
+  Roaring r1;
+  for (uint32_t i = 100; i < 1000; i++) {
+    r1.add(i);
+  }
+  std::cout << "cardinality = " << r1.cardinality() << std::endl;
+
+  Roaring64Map r2;
+  for (uint64_t i = 18000000000000000100ull; i < 18000000000000001000ull; i++) {
+    r2.add(i);
+  }
+  std::cout << "cardinality = " << r2.cardinality() << std::endl;
+  return 0;
+}
+
